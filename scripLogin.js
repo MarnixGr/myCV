93ae6773fd9c684bef7a45bc255886ef61586aa1
Marnix <146172776+MarnixGr@users.noreply.github.com>
2023-10-19 22:32:26 +0100

    Update scripLogin.js

--- scripLogin.js
+++ scripLogin.js
@@ -1,7 +1,7 @@
 document.getElementById("passwordForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var password = document.getElementById("password").value;
-    if (password === "your_password_here") {
+    if (password === "Chickens!") {
         document.getElementById("message").textContent = "Access Granted";
     } else {
         document.getElementById("message").textContent = "Access Denied";
